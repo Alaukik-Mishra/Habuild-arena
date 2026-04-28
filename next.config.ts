d77c7e490982cb1592a3c360f8af7484fb7c4767
Alaukik-Mishra <alaukikmishra70@gmail.com>
2026-04-28 22:34:13 +0530

    redirect next config

--- next.config.ts
+++ next.config.ts
@@ -13,6 +13,22 @@ const nextConfig: NextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
+  // --- ADD THIS SECTION ---
+  async redirects() {
+    return [
+      {
+        source: '/:path*',
+        has: [
+          {
+            type: 'host',
+            value: 'habuild-arena-q69fisr1o-shlok-mishras-projects.vercel.app',
+          },
+        ],
+        destination: 'https://habuild-arena.vercel.app/:path*',
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
